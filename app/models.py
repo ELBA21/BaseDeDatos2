aea3b39197b16d2abc811b50a5b040ca58693294
Andres Hernandez <andresfernando.hernandez@alumnos.ulagos.cl>
2025-06-12 17:25:04 -0400

    se hace primera migración

--- app/models.py
+++ app/models.py
@@ -1,17 +1,2 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Time
 from .db import Base
-
-
-class EjemploRelacion(Base):
-    __tablename__ = "ejemplo_relacion"
-
-    id = Column(Integer, primary_key=True, index=True)
-    nombre = Column(String, index=True)
-    descripcion = Column(String, index=True)
-    tiempo = Column(Time, nullable=False)
-
-    # Clave foranea a otro modelo
-    otro_id = Column(Integer, ForeignKey("otro_modelo.id"))
-
-    # Relación con otro modelo (descomentar si es necesario)
-    # otro_modelo = relationship("OtroModelo", back_populates="ejemplo_relacion")
