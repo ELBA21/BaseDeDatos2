17038138167c7496c8fe83970297be2040925dbd
Elba <benjaminalberto.martinez@alumnos.ulagos.cl>
2025-06-14 23:00:03 -0400

    Test primera tabla

--- app/models.py
+++ app/models.py
@@ -1,2 +1,11 @@
 from sqlalchemy import Column, Integer, String, ForeignKey, Time
 from .db import Base
+
+
+class Asociacion(Base):
+    __tablename__ = 'Asociacion'
+
+    id = Column(Integer, primary_key=True, index=True)
+    nombre = Column(String, nullable=False)
+    ciudad = Column(String, nullable=False)
+    pais = Column(String, nullable=False)
