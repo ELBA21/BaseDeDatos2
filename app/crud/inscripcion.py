12f14a3a4fec669dd81f843132a72a0f3f432e52
Andres Hernandez <andresfernando.hernandez@alumnos.ulagos.cl>
2025-06-27 21:28:52 -0400

    arreglé el ingreso de datos de Inscripcion

--- app/crud/inscripcion.py
+++ app/crud/inscripcion.py
@@ -4,16 +4,21 @@ from ..models import Inscripcion, Jugador, Equipo, TorneoCategoria
 
 ###ESTE LO ISE YO COPIANDOME AMI MISMO OWO AWA EWE, AWA QUE PARECE EWE PERO SABE A UWU
 
-def create_inscripcion(session: Session, jugador_id: int, equipo_id: int, torneo_categoria_id: int):
+def create_inscripcion(session: Session, torneo_categoria_id: int, jugador_id: Optional[int] = None, equipo_id: Optional[int] = None):
     # Validar que el jugador existe
-    jugador = session.query(Jugador).get(jugador_id)
-    if not jugador:
-        raise ValueError(f"Jugador con ID {jugador_id} no existe")
+
+    if(jugador_id is None):
+        equipo = session.query(Equipo).get(equipo_id)
+        if not equipo:
+            raise ValueError(f"Equipo con ID {equipo_id} no existe")
+        
+    if(equipo_id is None):
+        jugador = session.query(Jugador).get(jugador_id)
+        if not jugador:
+            raise ValueError(f"Jugador con ID {jugador_id} no existe")
 
     # Validar que la equipo existe
-    equipo = session.query(Equipo).get(equipo_id)
-    if not equipo:
-        raise ValueError(f"Equipo con ID {equipo_id} no existe")
+    
     
     torneo_categoria = session.query(TorneoCategoria).get(torneo_categoria_id)
     if not torneo_categoria:
@@ -25,7 +30,7 @@ def create_inscripcion(session: Session, jugador_id: int, equipo_id: int, torneo
     ).first()
 
     if existe:
-        return existe  # o lanzar un error si no quieres duplicados
+        raise ValueError(f"El equipo o el jugador ya estan inscritos")
 
     nueva = Inscripcion(jugador_id=jugador_id, equipo_id=equipo_id, torneo_categoria_id=torneo_categoria_id)
     session.add(nueva)
