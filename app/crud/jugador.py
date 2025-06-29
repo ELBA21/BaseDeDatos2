8187340b0235040314e60526dfa6616d9b3e0a7b
Nurodayo <cianuroicecream@gmail.com>
2025-06-29 19:39:12 -0400

    jugador validations y quize me heche el input de la fecha pq ahora si no le pones YYYY-MM-DD se enoja D:

--- app/crud/jugador.py
+++ app/crud/jugador.py
@@ -1,42 +1,84 @@
 from datetime import date
 from typing import Optional
 from sqlalchemy.orm import Session
-from ..models import Jugador, Asociacion,Categoria
-
-def create_jugador(session: Session, nombre:str, fecha_nacimiento:date, genero:str, pais:str, ciudad:str, asociacion_id:int, categoria_id:int):
-    jugador = Jugador(nombre=nombre, fecha_nacimiento=fecha_nacimiento, genero=genero, pais=pais, ciudad=ciudad, asociacion_id=asociacion_id, categoria_id=categoria_id)
-    session.add(jugador)
-    session.commit()
-    return jugador
+from ..models import Jugador, Asociacion, Categoria
 
 
-def get_jugador_id(session: Session, jugador_id:int):
+def create_jugador(
+    session: Session,
+    nombre: str,
+    fecha_nacimiento: date,
+    genero: str,
+    pais: str,
+    ciudad: str,
+    categoria_id: int,
+    asociacion_id: Optional[int],
+):
+    today = date.today()
+    if fecha_nacimiento < today:
+        jugador = Jugador(
+            nombre=nombre,
+            fecha_nacimiento=fecha_nacimiento,
+            genero=genero,
+            pais=pais,
+            ciudad=ciudad,
+            asociacion_id=asociacion_id,
+            categoria_id=categoria_id,
+        )
+        session.add(jugador)
+        session.commit()
+        return jugador
+    else:
+        print("No se admiten paradojas temporales")
+
+
+def get_jugador_id(session: Session, jugador_id: int):
     jugador = session.get(Jugador, jugador_id)
     return jugador
 
-def update_jugador_id(session: Session, jugador_id:int, nombre:Optional[str]=None, fecha_nacimiento:Optional[date]=None, genero:Optional[str]=None, pais:Optional[str]=None, ciudad:Optional[str]=None, asociacion_id:Optional[int]=None, categoria_id:Optional[int]=None):
+
+def update_jugador_id(
+    session: Session,
+    jugador_id: int,
+    nombre: Optional[str] = None,
+    fecha_nacimiento: Optional[date] = None,
+    genero: Optional[str] = None,
+    pais: Optional[str] = None,
+    ciudad: Optional[str] = None,
+    asociacion_id: Optional[int] = None,
+    categoria_id: Optional[int] = None,
+):
     jugador = session.get(Jugador, jugador_id)
     if not jugador:
         print("NO ENCONTRADO")
         return None
     if nombre is not None:
         jugador.nombre = nombre
+    today = date.today()
     if fecha_nacimiento is not None:
-        jugador.fecha_nacimiento = fecha_nacimiento
-    if genero is not None:
+        if fecha_nacimiento < today:
+            jugador.fecha_nacimiento = fecha_nacimiento
+        else:
+            print("No se admiten viajeros en el tiempo")
+    if (
+        genero is not None
+    ):  # hay que ser progresivos, que pasa si alguien no tiene genero, lo dejare asi porque demas que el profe dice q no,
+        # pero weno los generos paises y ciudades deberian ser tablas apartes mis amigos programadores y benjamin martinez,
+        # se que nadie va a leer esto
         jugador.genero = genero
     if pais is not None:
         jugador.pais = pais
     if ciudad is not None:
         jugador.ciudad = ciudad
-    if asociacion_id is not None:
-        jugador.asociacion_id = asociacion_id
+    # if asociacion_id is not None:  # un jugador si puede tener None en categoria
+    jugador.asociacion_id = asociacion_id
     if categoria_id is not None:
         jugador.categoria_id = categoria_id
     session.commit()
     return jugador
 
-def delete_jugador(session: Session, jugador_id:int):
+
+def delete_jugador(session: Session, jugador_id: int):
     jugador = session.get(Jugador, jugador_id)
     if not jugador:
         print("NO ENCONTRADO")
@@ -44,3 +86,4 @@ def delete_jugador(session: Session, jugador_id:int):
     session.delete(jugador)
     session.commit()
     return jugador
+
--- app/router/jugador.py
+++ app/router/jugador.py
@@ -1,61 +1,117 @@
-from fastapi  import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from ..db import get_db
-from ..crud.jugador import (get_jugador_id, create_jugador, update_jugador_id, delete_jugador)
+from ..crud.jugador import (
+    get_jugador_id,
+    create_jugador,
+    update_jugador_id,
+    delete_jugador,
+)
 from sqlalchemy.orm import Session
 from typing import Optional
+from datetime import date
+
 router = APIRouter()
 
 
 @router.post("/")
-def create_jugador_endpoint(nombre: str, fecha_nacimiento: str, genero: str, pais: str, ciudad: str, asociacion_id: int, categoria_id: int, session: Session = Depends(get_db)):
-    jugador = create_jugador(session, nombre, fecha_nacimiento, genero, pais, ciudad, asociacion_id, categoria_id)
-    return {"id": jugador.id,
-            "nombre": jugador.nombre,
-            "fecha_nacimiento": jugador.fecha_nacimiento,
-            "genero": jugador.genero,
-            "pais": jugador.pais,
-            "ciudad": jugador.ciudad,
-            "asociacion_id": jugador.asociacion_id,
-            "categoria_id": jugador.categoria_id}
+def create_jugador_endpoint(
+    nombre: str,
+    fecha_nacimiento: date,
+    genero: str,
+    pais: str,
+    ciudad: str,
+    categoria_id: int,
+    asociacion_id: Optional[int] = None,
+    session: Session = Depends(get_db),
+):
+    jugador = create_jugador(
+        session,
+        nombre,
+        fecha_nacimiento,
+        genero,
+        pais,
+        ciudad,
+        categoria_id,
+        asociacion_id,
+    )
+    return {
+        "id": jugador.id,
+        "nombre": jugador.nombre,
+        "fecha_nacimiento": jugador.fecha_nacimiento,
+        "genero": jugador.genero,
+        "pais": jugador.pais,
+        "ciudad": jugador.ciudad,
+        "categoria_id": jugador.categoria_id,
+        "asociacion_id": jugador.asociacion_id,
+    }
+
 
 @router.get("/{jugador_id}")
-def get_jugador_id_endpoint(jugador_id:int, session:Session = Depends(get_db)):
+def get_jugador_id_endpoint(jugador_id: int, session: Session = Depends(get_db)):
     jugador = get_jugador_id(session, jugador_id)
     if not jugador:
         raise HTTPException(status_code=404, detail="Jugador No Encontrado")
-    return {"id": jugador.id,
-            "nombre": jugador.nombre,
-            "fecha_nacimiento": jugador.fecha_nacimiento,
-            "genero": jugador.genero,
-            "pais": jugador.pais,
-            "ciudad": jugador.ciudad,
-            "asociacion_id": jugador.asociacion_id,
-            "categoria_id": jugador.categoria_id}
+    return {
+        "id": jugador.id,
+        "nombre": jugador.nombre,
+        "fecha_nacimiento": jugador.fecha_nacimiento,
+        "genero": jugador.genero,
+        "pais": jugador.pais,
+        "ciudad": jugador.ciudad,
+        "asociacion_id": jugador.asociacion_id,
+        "categoria_id": jugador.categoria_id,
+    }
+
 
 @router.put("/{jugador_id}")
-def update_jugador_id_endpoint(jugador_id:int, nombre:Optional[str]=None, fecha_nacimiento:Optional[str]=None, genero:Optional[str]=None, pais:Optional[str]=None,ciudad:Optional[str]=None, asociacion_id:Optional[int]=None, categoria_id:Optional[int]=None, session:Session = Depends(get_db)):
-    jugador = update_jugador_id(session, jugador_id, nombre, fecha_nacimiento, genero, pais, ciudad, asociacion_id, categoria_id)
+def update_jugador_id_endpoint(
+    jugador_id: int,
+    nombre: Optional[str] = None,
+    fecha_nacimiento: Optional[str] = None,
+    genero: Optional[str] = None,
+    pais: Optional[str] = None,
+    ciudad: Optional[str] = None,
+    asociacion_id: Optional[int] = None,
+    categoria_id: Optional[int] = None,
+    session: Session = Depends(get_db),
+):
+    jugador = update_jugador_id(
+        session,
+        jugador_id,
+        nombre,
+        fecha_nacimiento,
+        genero,
+        pais,
+        ciudad,
+        asociacion_id,
+        categoria_id,
+    )
     if not jugador:
         raise HTTPException(status_code=404, detail="Jugador No Encontrado")
-    return {"id": jugador.id,
-            "nombre": jugador.nombre,
-            "fecha_nacimiento": jugador.fecha_nacimiento,
-            "genero": jugador.genero,
-            "pais": jugador.pais,
-            "ciudad": jugador.ciudad,
-            "asociacion_id": jugador.asociacion_id,
-            "categoria_id": jugador.categoria_id}
+    return {
+        "id": jugador.id,
+        "nombre": jugador.nombre,
+        "fecha_nacimiento": jugador.fecha_nacimiento,
+        "genero": jugador.genero,
+        "pais": jugador.pais,
+        "ciudad": jugador.ciudad,
+        "asociacion_id": jugador.asociacion_id,
+        "categoria_id": jugador.categoria_id,
+    }
+
 
 @router.delete("/{jugador_id}")
-def delete_jugador_endpoint(jugador_id:int, session:Session = Depends(get_db)):
+def delete_jugador_endpoint(jugador_id: int, session: Session = Depends(get_db)):
     jugador = delete_jugador(session, jugador_id)
     if not jugador:
         raise HTTPException(status_code=404, detail="Jugador No Encontrado")
-    return {"id": jugador.id,
-            "nombre": jugador.nombre,
-            "fecha_nacimiento": jugador.fecha_nacimiento,
-            "genero": jugador.genero,
-            "pais": jugador.pais,
-            "ciudad": jugador.ciudad,
-            "asociacion_id": jugador.asociacion_id,
-            "categoria_id": jugador.categoria_id}
+    return {
+        "id": jugador.id,
+        "nombre": jugador.nombre,
+        "fecha_nacimiento": jugador.fecha_nacimiento,
+        "genero": jugador.genero,
+        "pais": jugador.pais,
+        "ciudad": jugador.ciudad,
+        "asociacion_id": jugador.asociacion_id,
+        "categoria_id": jugador.categoria_id,
+    }
