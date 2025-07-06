7db266247e2e9d3f453c6646a2eac5589a9d3ee8
Nurodayo <cianuroicecream@gmail.com>
2025-07-05 21:39:59 -0400

    antes de q me lo heche todo refine las validaciones de partido

--- app/crud/partido.py
+++ app/crud/partido.py
@@ -25,18 +25,21 @@ def create_partido(
         and jugador1_id is None
         and jugador2_id is None
     ):  # if qliao de yandere dev
-        partido = Partido(
-            horario=horario,
-            equipo1_id=equipo1_id,
-            equipo2_id=equipo2_id,
-            equipo_ganador_id=equipo_ganador_id,
-            jugador1_id=None,
-            jugador2_id=None,
-            jugador_ganador_id=None,
-            categoria_id=categoria_id,
-            mesa_id=mesa_id,
-            fase_id=fase_id,
-        )
+        if (
+            equipo2_id is not equipo1_id
+        ):  # basicamente validando que no haya un partido de uno contra si mismo
+            partido = Partido(
+                horario=horario,
+                equipo1_id=equipo1_id,
+                equipo2_id=equipo2_id,
+                equipo_ganador_id=equipo_ganador_id,
+                jugador1_id=None,
+                jugador2_id=None,
+                jugador_ganador_id=None,
+                categoria_id=categoria_id,
+                mesa_id=mesa_id,
+                fase_id=fase_id,
+            )
         # session.add(partido)
         # session.commit()
         # return partido
@@ -46,18 +49,19 @@ def create_partido(
         and equipo1_id is None
         and equipo2_id is None
     ):
-        partido = Partido(
-            horario=horario,
-            equipo1_id=None,
-            equipo2_id=None,
-            equipo_ganador_id=None,
-            jugador1_id=jugador1_id,
-            jugador2_id=jugador2_id,
-            jugador_ganador_id=jugador_ganador_id,
-            categoria_id=categoria_id,
-            mesa_id=mesa_id,
-            fase_id=fase_id,
-        )
+        if jugador2_id is not jugador1_id:
+            partido = Partido(
+                horario=horario,
+                equipo1_id=None,
+                equipo2_id=None,
+                equipo_ganador_id=None,
+                jugador1_id=jugador1_id,
+                jugador2_id=jugador2_id,
+                jugador_ganador_id=jugador_ganador_id,
+                categoria_id=categoria_id,
+                mesa_id=mesa_id,
+                fase_id=fase_id,
+            )  # IF: IF: IF: IF: es python asi que no me importa la eficiencia
     # session.add(partido)
     # session.commit()
     # return partido
@@ -93,18 +97,23 @@ def update_partido_id(
         return None
     if horario is not None:
         partido.horario = horario
-    if equipo1_id is not None:
-        partido.equipo1_id = equipo1_id
-    if equipo2_id is not None:
-        partido.equipo2_id = equipo2_id
-    if equipo_ganador_id is not None:
-        partido.equipo_ganador_id = equipo_ganador_id
-    if jugador1_id is not None:
-        partido.jugador1_id = jugador1_id
-    if jugador2_id is not None:
-        partido.jugador2_id = jugador2_id
-    if jugador_ganador_id is not None:
-        partido.jugador_ganador_id = jugador_ganador_id
+    # Solo checkeo uno porque a la hora de crear el objeto o hay dos equipos o hay dos jugadores
+    # asi que si el equipo 1 no es null el equipo 2 tampoco lo sera y pasara lo mismo con los jugadores
+    if partido.equipo1_id is not None and (equipo1_id is not equipo2_id):
+        if equipo1_id is not None:
+            partido.equipo1_id = equipo1_id
+        if equipo2_id is not None:
+            partido.equipo2_id = equipo2_id
+        if equipo_ganador_id is not None:
+            partido.equipo_ganador_id = equipo_ganador_id
+    elif partido.jugador1 is not None and (jugador1_id is not jugador2_id):
+        if jugador1_id is not None:
+            partido.jugador1_id = jugador1_id
+        if jugador2_id is not None:
+            partido.jugador2_id = jugador2_id
+        if jugador_ganador_id is not None:
+            partido.jugador_ganador_id = jugador_ganador_id
+
     if categoria_id is not None:
         partido.categoria_id = categoria_id
     if mesa_id is not None:
