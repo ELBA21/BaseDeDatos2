db2b9223f75b2bf5bdec2c8a933a0da50ebbd395
Nurodayo <cianuroicecream@gmail.com>
2025-07-10 15:53:26 -0400

    ciudad router listo... creo

--- app/router/ciudad.py
+++ app/router/ciudad.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, Depends, HTTPException
 from ..db import get_db
 from ..crud.ciudad import create_ciudad, get_ciudad_id, update_ciudad_id, delete_ciudad
+from ..crud.pais import get_pais_id
 from sqlalchemy.orm import Session, session
 from typing import Optional
 from datetime import date
@@ -15,6 +16,9 @@ def create_ciudad_endpoint(
     pais: int,
     session: Session = Depends(get_db),
 ):
+    pais_id = get_pais_id(session, pais)
+    if not pais_id:
+        raise HTTPException(status_code=404, detail="Pais no encontrado")
     ciudad = create_ciudad(session, nombre, pais)
     return {
         "id": ciudad.id,
@@ -45,6 +49,10 @@ def update_ciudad_id_endpoint(
     pais: Optional[int] = None,
     session: Session = Depends(get_db),
 ):
+    if pais is not None:
+        pais_id = get_pais_id(session, pais)
+        if not pais_id:
+            raise HTTPException(status_code=404, detail="Pais no encontrado")
     ciudad = update_ciudad_id(session, ciudad_id, nombre, pais)
     if not ciudad:
         raise HTTPException(status_code=404, detail="Ciudad no encontrado")
